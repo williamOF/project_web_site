e2c8db076af870785bae936bfbb6d66d797a30d9
myke william <maike.study@gmail.com>
2022-10-28 12:51:29 -0300

    header-html

---
+++ controllers/homeController.js
@@ -0,0 +1,5 @@
+const home = (req,res) =>{
+    return res.render('index')
+};
+
+module.exports = home
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+//controllers is required
+const homeController = require('../controllers/homeController')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/', homeController )
 
 module.exports = router;
